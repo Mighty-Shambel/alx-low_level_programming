3c6eb3d4d4af79baefac6d8b9ff16634904a9a58
Mighty-Shambel <mightyshambel797@gmail.com>
2022-09-12 16:00:06 -0700

    The 7th msg

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,79 @@
+Task 7.txt
+
+ማን መዳረሻ እንዳለው
+
+A
+
+የስርዓት ባህሪያት
+
+ዓይነት
+
+ጽሁፍ
+
+መጠን
+
+325 ባይት
+
+ጥቅም ላይ የዋለው ማከማቻ
+
+325 ባይት
+
+አካባቢ
+
+Variables if else while
+
+ባለቤት
+
+Antony Bahati
+
+የተቀየረው
+
+10 ሴፕቴ 2022 በAntony Bahati
+
+የተከፈተው
+
+1:58 ጥዋት በእኔ
+
+የተፈጠረው
+
+10 ሴፕቴ 2022
+
+መግለጫ የለም
+
+ተመልካቾች ማውረድ ይችላሉ
+
+#include <stdio.h>
+
+/**
+
+ * main - print if the number is postive, zero, or negative
+
+ *
+
+ * Description: using the main function
+
+ * this program prints "Programming is positive, zero, or negative
+
+ * Return: 0
+
+ */
+
+int main(void)
+  
+{
+  
+  char c;
+  
+  for (c = 'z'; c >= 'a'; c--)
+    
+    {
+      
+      putchar(c);
+      
+    }
+  
+  putchar('\n');
+  
+  return (0);
+  
+}
