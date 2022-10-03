2c30ecce38eadf48154c9813a8eaf197b247a090
Mighty-Shambel <mightyshambel797@gmail.com>
2022-10-03 22:02:43 +0300

    the first push

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,19 @@
-#include <stdio.h>                                                                                                                   
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                      * main - Print the name of the program                                                                                  * @argc: Count arguments                                                                                                * @argv: Arguments                                                                                                   
- *
- * Return:Always 0 (Success)
- */                                                                  
+#include<stdio.h>
 
-int main(int argc, char *argv[])                                                                                        {                                                                                                                      
-(void) argc;                                                         
-printf("%s\n", argv[0]);                                                                                                             
-	                                                                                                                return (0);                                                                                                             }
+/**
+ * main - prints the name of the program
+ *   
+ * @argc: number of command line arguments or size
+ * of the array argv
+ * @argv: The array name that holds the command line arguments
+ *       
+ * Return: returns 0 with success
+ *         
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc)
+		printf("%s\n", *argv);
+	return (0);
+}
